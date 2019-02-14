178650e8780e2c2f7014f2cb691f0198ce200cc3
Angel Castillo <32829371+ACast59@users.noreply.github.com>
2019-02-14 03:57:37 -0400

    Create random.js

---
+++ js/random.js
@@ -0,0 +1,26 @@
+function showLucky(root){
+	var feed = root.feed;
+	var entries = feed.entry || [];
+	var entry = feed.entry[0];
+	
+	for (var j = 0; j < entry.link.length; ++j) {
+		if (entry.link[j].rel == 'alternate') {
+			window.location = entry.link[j].href;
+		}
+	}
+}
+
+function fetchLuck(luck) {
+	script = document.createElement('script');
+	script.src = '/feeds/posts/summary?start-index='+luck+'&max-results=1&alt=json-in-script&callback=showLucky';
+	script.type = 'text/javascript';
+	document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+function feelingLucky(root) {
+	var feed = root.feed;
+	var total = parseInt(feed.openSearch$totalResults.$t,10);
+	var luckyNumber = Math.floor(Math.random()*total);luckyNumber++;
+	document.getElementById('hm-random').rel = luckyNumber;
+	document.getElementById('hm-random').onclick = function(){fetchLuck(this.rel);};
+}
